bdea76a1170cc12193bba52f2035e913ac092af0
pritiYadav1 <115352601+pritiYadav1@users.noreply.github.com>
2025-07-08 17:33:31 +0530

    Add files via upload

---
+++ Operators_demo.py
@@ -0,0 +1,21 @@
+num1=10
+num2=15
+num4= 20
+result=num1+num2
+print(result)
+
+result1=(num1<num2)
+print(result1)
+
+result2=(num1<num2 or num2==num1)
+print(result2)
+
+num3=num1
+num4= num4 + num3
+print(num4)
+
+result=num1 ^ num2
+print(result)
+
+
+
