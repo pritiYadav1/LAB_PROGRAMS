5be8ea956a6480b1700c6f8bcc5aba96410a9335
pritiYadav1 <115352601+pritiYadav1@users.noreply.github.com>
2025-07-08 17:32:15 +0530

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,12 @@
+#my user data input program
+'''
+This is multline comments
+'''
+studentName=input("Enter your name")
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+print(f"your name is ={studentName}")
+print("Addition is =",num1+num2)
+result=num1+num2
+print(f"addition of {num1} and {num2} = {result}")
+
